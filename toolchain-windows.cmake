31f860fbc8be97af21c593cd8c81c2b3fb44ed54
cowie <cowiedan2007@icloud.com>
2026-04-24 12:52:17 +0100

    Changes to worker to build

---
+++ toolchain-windows.cmake
@@ -0,0 +1,18 @@
+# Windows MinGW-w64 Toolchain for macOS
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# Specify the cross compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# Don't try to run cross-compiled executables
+set(CMAKE_CROSSCOMPILING_EMULATOR "")
+
+# Search for programs in the host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# Search for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
